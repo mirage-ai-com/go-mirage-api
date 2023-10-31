59e87d881afc60370750a3492b34f831aa5759b3
Valerian Saliou <valerian@valeriansaliou.name>
2023-10-31 11:47:26 +0100

    Add context ingest routes

--- examples/data_context_ingest/main.go
+++ examples/data_context_ingest/main.go
@@ -18,23 +18,32 @@ const (
 func main() {
   client := mirage.New(CONFIG_USER_ID, CONFIG_SECRET_KEY)
 
+  // Generate context ingest data
+  ingestSecondaryID := "sec_6693a4a2-e33f-4cce-ba90-b7b5b0922c46"
+  ingestTertiaryID := "ter_de2bd6e7-74e1-440d-9a23-01964cd4b7da"
+  ingestText := "Text to index here..."
+  ingestSource := "chat"
+  ingestTimestamp := uint64(1682002198552)
+
+  ingestMetadata := map[string]string{
+    "custom_key": "custom_value",
+    "another_key": "another_value",
+  }
+
   // Ingest provided context data
   data, err := client.Task.IngestContextData(mirage.IngestContextDataRequest {
     Items: []mirage.IngestContextDataRequestItem {
       mirage.IngestContextDataRequestItem {
         Operation: "index",
         PrimaryID: "pri_cf44dd72-4ba9-4754-8fb3-83c4261243c4",
-        SecondaryID: "sec_6693a4a2-e33f-4cce-ba90-b7b5b0922c46",
-        TertiaryID: "ter_de2bd6e7-74e1-440d-9a23-01964cd4b7da",
+        SecondaryID: &ingestSecondaryID,
+        TertiaryID: &ingestTertiaryID,
 
-        Text: "Text to index here...",
-        Source: "chat",
-        Timestamp: 1682002198552,
+        Text: &ingestText,
+        Source: &ingestSource,
+        Timestamp: &ingestTimestamp,
 
-        Metadata: {
-          CustomKey: "custom_value",
-          AnotherKey: "another_value",
-        },
+        Metadata: &ingestMetadata,
       },
     },
   })
---
+++ mirage/data_context.go
@@ -0,0 +1,55 @@
+// go-mirage-api
+//
+// Copyright 2023, Valerian Saliou
+// Author: Valerian Saliou <valerian@valeriansaliou.name>
+
+package mirage
+
+
+// IngestContextDataRequest mapping
+type IngestContextDataRequest struct {
+  Items  []IngestContextDataRequestItem  `json:"items"`
+}
+
+// IngestContextDataRequestItem mapping
+type IngestContextDataRequestItem struct {
+  Operation     string              `json:"operation"`
+  PrimaryID     string              `json:"primary_id"`
+  SecondaryID   *string             `json:"secondary_id,omitempty"`
+  TertiaryID    *string             `json:"tertiary_id,omitempty"`
+  Text          *string             `json:"text,omitempty"`
+  Timestamp     *uint64             `json:"timestamp,omitempty"`
+  Source        *string             `json:"source,omitempty"`
+  Metadata      *map[string]string  `json:"metadata,omitempty"`
+}
+
+
+// IngestContextDataResponseData mapping
+type IngestContextDataResponseData struct {
+  Data  *IngestContextDataResponse  `json:"data"`
+}
+
+// IngestContextDataResponse mapping
+type IngestContextDataResponse struct {
+  Imported  bool  `json:"imported"`
+}
+
+
+// String returns the string representation of IngestContextDataResponse
+func (instance IngestContextDataResponse) String() string {
+  return Stringify(instance)
+}
+
+
+// IngestContextData ingest context data into account.
+func (service *TaskService) IngestContextData(data IngestContextDataRequest) (*IngestContextDataResponse, error) {
+  req, _ := service.client.NewRequest("POST", "data/context/ingest", data)
+
+  result := new(IngestContextDataResponseData)
+  _, err := service.client.Do(req, result)
+  if err != nil {
+    return nil, err
+  }
+
+  return result.Data, err
+}
