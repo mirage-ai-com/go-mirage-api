34950515dca6d8b033ff1d46f348f961227ef40d
Valerian Saliou <valerian@valeriansaliou.name>
2023-10-31 21:25:34 +0100

    v1.3.0

--- mirage/mirage.go
+++ mirage/mirage.go
@@ -20,7 +20,7 @@ import (
 
 
 const (
-  libraryVersion = "1.2.0"
+  libraryVersion = "1.3.0"
   defaultRestEndpointURL = "https://api.mirage-ai.com/v1/"
   userAgent = "go-mirage-api/" + libraryVersion
   acceptContentType = "application/json"
