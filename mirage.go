ebb0538c144ae06f852fbd15472c555b150822b6
Baptiste Jamin <baptiste@crisp.chat>
2024-11-18 14:36:33 +0100

    v1.7.4

--- mirage.go
+++ mirage.go
@@ -20,7 +20,7 @@ import (
 
 
 const (
-  libraryVersion = "1.7.3"
+  libraryVersion = "1.7.4"
   defaultRestEndpointURL = "https://api.mirage-ai.com/v1/"
   userAgent = "go-mirage-api/" + libraryVersion
   acceptContentType = "application/json"
