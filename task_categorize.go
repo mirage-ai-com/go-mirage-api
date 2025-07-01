485275312c20d38a13b32abc578f20462c70123e
Baptiste Jamin <baptiste@crisp.chat>
2025-07-01 14:24:54 +0200

    Categorize question

--- task_categorize.go
+++ task_categorize.go
@@ -36,11 +36,44 @@ func (instance CategorizeConversationsResponse) String() string {
 	return Stringify(instance)
 }
 
+// CategorizeQuestionRequest mapping
+type CategorizeQuestionRequest struct {
+	Question string `json:"question"`
+}
+
+// CategorizeQuestionResponseData mapping
+type CategorizeQuestionResponseData struct {
+	Data *CategorizeQuestionResponse `json:"data"`
+}
+
+// CategorizeQuestionResponse mapping
+type CategorizeQuestionResponse struct {
+	Category string `json:"category"`
+}
+
+// String returns the string representation of CategorizeQuestionResponse
+func (instance CategorizeQuestionResponse) String() string {
+	return Stringify(instance)
+}
+
 // CategorizeConversations categorize multiple conversations, from a list of messages for each individual conversation.
 func (service *TaskService) CategorizeConversations(ctx RequestContext, data CategorizeConversationsRequest) (*CategorizeConversationsResponse, error) {
 	req, _ := service.client.NewRequest("POST", "task/categorize/conversations", data, ctx)
 
 	result := new(CategorizeConversationsResponseData)
+	_, err := service.client.Do(req, result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.Data, err
+}
+
+// CategorizeQuestion categorize a single question, from a list of messages.
+func (service *TaskService) CategorizeQuestion(ctx RequestContext, data CategorizeQuestionRequest) (*CategorizeQuestionResponse, error) {
+	req, _ := service.client.NewRequest("POST", "task/categorize/question", data, ctx)
+
+	result := new(CategorizeQuestionResponseData)
 	_, err := service.client.Do(req, result)
 	if err != nil {
 		return nil, err
